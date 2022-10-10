87dd6093b09aec5c094c27fad55639535a0495cc
ealoticopve <ricardodavid120@gmail.com>
2022-10-10 21:12:31 +0100

    | Frontend | Chat Send Messages/Photos - v1

--- fend/src/components/Chat.jsx
+++ fend/src/components/Chat.jsx
@@ -4,6 +4,59 @@ import $ from 'jquery';
 import birdPhoto from '../public/photo.jpg'
 
 export default function Chat() {
+    const messages = [
+        {
+            user: 'Andreia Farinha',
+            messages: [
+                {
+                    msg: 'Olá Ricardo, tudo bem?',
+                    timestamp: '17:39'
+                }],
+        },
+        {
+            user: 'Ricardo Silva',
+            messages: [
+                {
+                    msg: 'Olá Andreia, está tudo bem e contigo?',
+                    timestamp: '17:41'
+                },
+                {
+                    msg: 'Esperei pela tua mensagem o dia inteiro :)',
+                    timestamp: '17:41'
+                },
+            ],
+        },
+        {
+            user: 'Andreia Farinha',
+            messages: [
+                {
+                    msg: 'Só consegui um tempinho agora... Mas já aqui estou :)',
+                    timestamp: '17:43'
+                }, {
+                    msg: 'Como foi o teu dia?',
+                    timestamp: '17:43'
+                },
+                {
+                    img: { birdPhoto },
+                    timestamp: '17:44'
+                }
+            ],
+        },
+        {
+            user: 'Ricardo Silva',
+            messages: [
+                {
+                    msg: 'Normal e o teu?',
+                    timestamp: '17:44'
+                },
+                {
+                    img: { birdPhoto },
+                    timestamp: '17:44'
+                }
+            ],
+        },
+    ]
+
     useEffect(() => {
         $(function () {
             setTimeout(() => {
@@ -17,40 +70,36 @@ export default function Chat() {
     return (
         <div className='chat'>
             <div id='messages' className='messages'>
-                <div className='msg-from '>
-                    <p className='text-msg'>
-                        Olá Ricardo, tudo bem?
-                    </p>
-                </div>
-                <div className='msg-to'>
-                    <p className='text-msg me'>
-                        Olá Andy, está tudo bem, obrigado!
-                        Vamos lá acabar a app :)
-                    </p>
-                </div>
-                <div className='msg-from '>
-                    <p className='text-msg'>
-                        Vamos lá! Falta pouco!
-                    </p>
-                </div>
-                <div className='msg-to'>
-                    <p className='text-msg me'>
-                        Ainda agora começamos...
-                    </p>
-                    <p className='text-msg me '>
-                        <img className='send-photo' src={birdPhoto} alt='birdPhoto' />
-                    </p>
-                </div>
-                <div className='msg-from '>
-                    <p className='text-msg'>
-                        Passarinho ?!
-                    </p>
-                </div>
-                <div className='msg-to'>
-                    <p className='text-msg me'>
-                        Ele não morde... não tem dentes
-                    </p>
-                </div>
+                {messages.map((message, i) => (
+                    <div key={i}>
+                        {message.user !== 'Ricardo Silva' ?
+                            <>
+                                {message.messages.map((msg, idx) => (
+                                    <div className='msg-from' key={idx} >
+                                        <p className='text-msg'>
+                                            {msg.img ?
+                                                <img className='send-photo' src={birdPhoto} alt='birdPhoto' /> :
+                                                <>{msg.msg}</>
+                                            }
+                                        </p>
+                                    </div>
+                                ))}
+                            </> :
+                            <>
+                                {message.messages.map((msg, idx) => (
+                                    <div className='msg-to' key={idx}>
+                                        <p className='text-msg me'>
+                                            {msg.img ?
+                                                <img className='send-photo' src={birdPhoto} alt='birdPhoto' /> :
+                                                <>{msg.msg}</>
+                                            }
+                                        </p>
+                                    </div>
+                                ))}
+                            </>
+                        }
+                    </div>
+                ))}
             </div>
             <NavbarMessaging />
         </div >
--- fend/src/components/NavbarMessaging.jsx
+++ fend/src/components/NavbarMessaging.jsx
@@ -18,21 +18,114 @@ const options = {
 export default function NavbarMessaging() {
     const [userOptions, setUserOptions] = useState(null);
     const [emojisAvailable, setEmojisAvailable] = useState(null);
+    const [msg, setMsg] = React.useState('');
+    const [src, setSrc] = React.useState('');
 
-    const handleOpenUserMenu = (event) => {
-        setUserOptions(event.currentTarget);
+    const handleOpenUserMenu = (e) => {
+        setUserOptions(e.currentTarget);
     };
 
     const handleCloseUserMenu = () => {
         setUserOptions(null);
     };
 
-    const handleOpenEmojis = (event) => {
-        setEmojisAvailable(event.currentTarget);
+    const handleOpenEmojis = (e) => {
+        setEmojisAvailable(e.currentTarget);
     };
 
     const handleCloseEmojis = () => {
         setEmojisAvailable(null);
+    };
+
+    const handleEmojiClick = (emoji) => {
+        setMsg((currMsg) => currMsg + emoji.emoji)
+    }
+
+    const handleMessage = (e) => {
+        setMsg(e.target.value);
+    }
+
+    const sendMessage = () => {
+        const msgs = document.getElementById('messages')
+        const msgContainer = document.createElement('div')
+        const newMessage = document.createElement('p')
+        msgContainer.className = 'msg-to'
+        newMessage.className = 'text-msg me'
+
+        if (msg !== '' && src !== '') {
+            sendImage(msgContainer, newMessage, msgs)
+
+            //Send Text too, but with a second element
+            const msgContainer2 = document.createElement('div')
+            const newMessage2 = document.createElement('p')
+
+            msgContainer2.className = 'msg-to'
+            newMessage2.className = 'text-msg me'
+
+            newMessage2.innerHTML = msg
+            msgContainer2.appendChild(newMessage2)
+            msgs.appendChild(msgContainer2)
+        }
+
+        else if (src !== '') {
+            sendImage(msgContainer, newMessage, msgs)
+        }
+
+        else
+            sendText(newMessage, msgContainer, msgs)
+
+        setTimeout(() => {
+            msgs.scrollTop = msgs.scrollHeight;
+            setSrc('')
+            setMsg('')
+        }, 100)
+    }
+
+    const sendImage = (msgContainer, newMessage, msgs) => {
+        const newImage = document.createElement('img')
+        newImage.src = src
+        newImage.style.width = '260px'
+        newMessage.appendChild(newImage)
+        msgContainer.appendChild(newMessage)
+        msgs.appendChild(msgContainer)
+
+        //delete temp img
+        document.getElementById('tempimg').remove()
+    }
+
+    const sendText = (newMessage, msgContainer, msgs) => {
+        newMessage.innerHTML = msg
+        msgContainer.appendChild(newMessage)
+        msgs.appendChild(msgContainer)
+    }
+
+    const handleEnter = (e) => {
+        if (e.key === 'Enter') {
+            sendMessage()
+        }
+    }
+
+    const handleFileSelect = function (file) {
+        var input = file.target;
+        var reader = new FileReader();
+        reader.onload = function () {
+            var dataURL = reader.result;
+            handleCloseUserMenu()
+            //sendMessage(this, dataURL)
+
+            //Show img to send in input
+            const showimg = document.getElementById('showimg-tosend')
+            const temp = document.createElement('img')
+            temp.src = dataURL
+            temp.style.width = '50px'
+            temp.id = 'tempimg'
+
+            showimg.appendChild(temp)
+            setSrc(dataURL)
+
+        };
+        reader.readAsDataURL(input.files[0]);
+
     };
 
     return (
@@ -41,12 +134,17 @@ export default function NavbarMessaging() {
             <div className='navbar-messaging'>
                 <div>
                     <Menu className='emoji-menu' anchorEl={emojisAvailable} open={Boolean(emojisAvailable)} onClose={handleCloseEmojis}>
-                        <MenuItem onClick={handleCloseEmojis}>
-                            <EmojiPicker theme='dark' />
+                        <MenuItem>
+                            <EmojiPicker
+                                theme='dark'
+                                onEmojiClick={handleEmojiClick}
+                                height={450}
+                                width={300}
+                                style={{ backgroundColor: 'transparent' }}
+                            />
                         </MenuItem>
                     </Menu>
                 </div>
-
                 <div className='send-options'>
                     <Button style={{ padding: 0 }}>
                         <Avatar style={{ background: '#fecb00', marginRight: '10px' }}>
@@ -61,19 +159,22 @@ export default function NavbarMessaging() {
                     <div>
                         <Menu anchorEl={userOptions} open={Boolean(userOptions)} onClose={handleCloseUserMenu}>
                             {Object.keys(options).map((key) => (
-                                <MenuItem key={key} onClick={handleCloseUserMenu}>
-                                    <Typography textAlign="center"> {options[key]} {key}</Typography>
+                                <MenuItem key={key} >
+                                    <Button component="label">
+                                        <Typography textAlign="center"> {options[key]} {key}</Typography>
+                                        <input hidden accept="image/*" type="file" id='selectedFile' onChange={handleFileSelect} />
+                                    </Button>
                                 </MenuItem>
                             ))}
                         </Menu>
                     </div>
-
                 </div>
                 <div className='send-message'>
-                    <Input placeholder="Write a message" style={{ width: '-webkit-fill-available' }} />
+                    <div id='showimg-tosend' style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', width: 'fit-content' }} />
+                    <Input value={msg} onChange={handleMessage} onKeyPress={handleEnter} placeholder="Write a message" style={{ width: '-webkit-fill-available' }} />
                 </div>
                 <div className='send-icon'>
-                    <Button>
+                    <Button onClick={sendMessage}>
                         <Icon color='#ff5252' icon="fluent:send-24-regular" width="50" />
                     </Button>
                 </div>
