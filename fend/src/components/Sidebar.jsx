8dfd553a666598394836b4f8c23ea4ca596bc926
ealoticopve <ricardodavid120@gmail.com>
2022-10-13 17:05:39 +0100

    section selected

--- fend/src/components/Sidebar.jsx
+++ fend/src/components/Sidebar.jsx
@@ -3,13 +3,26 @@ import List from '@mui/material/List';
 import { Icon } from '@iconify/react';
 import logo from '../public/logo.png'
 import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
 export default function Sidebar() {
+
+    const [sectionSelected, setSectionSelected] = useState('Chats')
     const firstList = {
-        NewChat: <Icon icon="carbon:add-filled" width="45" className='custom-red-icon' />,
-        Chats: <Icon icon="bi:chat" width="25" className='custom-grey-icon chat-icon' />,
-        Profile: <Icon icon="carbon:user" width="25" className='custom-grey-icon profile-icon' />,
-        Favs: <Icon icon="clarity:favorite-line" width="25" className='custom-grey-icon fav-icon' />,
-        Store: <Icon icon="clarity:store-line" width="25" className='custom-grey-icon store-icon' />,
+        NewChat: <Icon icon="carbon:add-filled" width="45" className='custom-red-default-icon pulse' />,
+        Chats: <Icon icon="bi:chat" width="25" className={`${sectionSelected === 'Chats' ? 'custom-red-icon' : 'custom-grey-icon'} chat-icon`} />,
+        Profile: <Icon icon="carbon:user" width="25" className={`${sectionSelected === 'Profile' ? 'custom-red-icon' : 'custom-grey-icon'} profile-icon`} />,
+        Favs: <Icon icon="clarity:favorite-line" width="25" className={`${sectionSelected === 'Favs' ? 'custom-red-icon' : 'custom-grey-icon'} fav-icon`} />,
+        Store: <Icon icon="clarity:store-line" width="25" className={`${sectionSelected === 'Store' ? 'custom-red-icon' : 'custom-grey-icon'} store-icon`} />,
+    }
+
+    const btnStyle = {
+        width: '100%',
+        height: '100%',
+        padding: 0,
+        borderRadius: '12px',
+        position: 'relative',
+        left: '-10px'
     }
 
     return (
@@ -20,7 +33,12 @@ export default function Sidebar() {
                 </div>
                 {Object.keys(firstList).map((icon, idx) => (
                     <div key={idx} className='iconsec'>
-                        {firstList[icon]}
+                        <Button variant="text"
+                            onClick={() => setSectionSelected(icon)}
+                            style={btnStyle}
+                            className={sectionSelected === icon && icon !== 'NewChat' ? 'active' : ''}>
+                            {firstList[icon]}
+                        </Button>
                     </div>
                 ))}
             </div>
